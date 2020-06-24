d7d81acf348b0f28381f4ae5489cadb79f012a82
yuanyi <yuanyi28@gmail.com>
2020-06-24 01:09:55 -0700

    Revised Debug in Login.js

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 //IP for Kubernetes
 // export const API_ROOT = 'http://34.68.233.18:8080';
 //IP for VM
-export const API_ROOT = 'http://35.193.13.223:8080/login';
+export const API_ROOT = 'http://35.193.13.223:8080';
 export const TOKEN_KEY = 'TOKEN';
 export const POSITION_KEY = 'POSITION';
 export const AUTH_HEADER = 'Bearer';
